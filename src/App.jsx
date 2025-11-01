a6b1836d0807f23b42b267db60c02617dc15b349
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-11-01 14:21:08 +0530

    Private Router file has been added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Auth/login";
 import NavBar from "./components/Navbar";
 import UserDashboard from "./components/User/UserDashboard";
 import Attributes from "./components/Attributes";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Login />} />
-          <Route path="/attributes" element={
+      <Routes>
+        <Route path="" element={<Login />} />
+        <Route
+          path="/attributes"
+          element={
+            <PrivateRoute>
               <NavBar />
-          }/>
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-        </Routes>
+              <Attributes />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/user-dashboard"
+          element={
+            <PrivateRoute>
+              <UserDashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
