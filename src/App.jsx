ce6b23bd643d317ae680ab472b4938856af7e9e3
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-11-04 09:45:31 +0530

    login token issue fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Auth/login";
@@ -15,7 +15,6 @@ import UserManagement from "./components/userManagement";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="" element={<Login />} />
         <Route
@@ -81,8 +80,6 @@ function App() {
           }
         />
       </Routes>
-      <ToastContainer position="top-right" autoClose={3000} />
-    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import ContextWrapper from "./context/ContextWrapper.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 createRoot(document.getElementById("root")).render(
-  <ContextWrapper>
-    <App />
-  </ContextWrapper>
+  <StrictMode>
+    <BrowserRouter>
+      <ContextWrapper>
+        <App />
+      </ContextWrapper>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </BrowserRouter>
+  </StrictMode>
+  
 );
